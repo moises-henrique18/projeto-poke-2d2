759b811c99281db3a8f1ccc5edd37266201deb27
moises-henrique18 <moiseshenrique.1810@aluno.barueri.br>
2025-05-07 08:30:07 -0300

    poke

---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import poke from './assets/pokeapi_256.png'
+const Home = () => {
+
+    return (
+        <div>
+            <img src={poke}></img>
+        </div>
+    )
+}
+
+export default Home
